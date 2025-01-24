f9f15389ccc8dc00258820f762d9e9aa04621df0
yue <artemisyuej@gmail.com>
2025-01-24 12:24:40 +0100

    放大镜-滑块跟随移动

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,51 +1,80 @@
 <script setup>
-import { ref } from 'vue';
+import { reactive, ref, watch } from 'vue';
+import { useMouseInElement } from '@vueuse/core';
 
+//图片列表
 const imageList = [
-    "https://via.placeholder.com/400?text=1",
-    "https://via.placeholder.com/400?text=2",
-    "https://via.placeholder.com/400?text=3",
-    "https://via.placeholder.com/400?text=4",
-    "https://via.placeholder.com/400?text=5",
+    "https://yanxuan-item.nosdn.127.net/3683d874b9623434a10b4ab0c2e6be9f.png",
+    "https://yanxuan-item.nosdn.127.net/87e67decbff7cc5b4d02050b8cd62d2f.png",
+    "https://yanxuan-item.nosdn.127.net/11956ee9b0657112819ad4b307cd6765.png",
+    "https://yanxuan-item.nosdn.127.net/6e76349dcd4f8e0ffe4d0d0960a17ad9.png",
+    "https://yanxuan-item.nosdn.127.net/0774f25b48c256a59e9e8f21e51fadae.png",
 ];
 
-//1.小图切换大图显示
-const activeIndex = ref(0);
-const enterhandler = (i) => {
-    activeIndex.value = i;
-}
-console.log(activeIndex);
+// 当前预览图的索引
+const currIndex = ref(0);
+
+// 1. 是否显示放大镜和大图
+const show = ref(false);
+
+// 2. 放大镜的坐标
+const layerPosition = reactive({
+    left: 0,
+    top: 0,
+});
+
+// 3. 使用useMouseInElement得到基于元素左上角的坐标和是否离开元素数据
+const target = ref(null);
+const { elementX, elementY, isOutside } = useMouseInElement(target);
+
+watch([elementX, elementY, isOutside], () => {
+    show.value = !isOutside.value;//在区域内才显示放大镜
+
+    const position = { x: 0, y: 0 }; //放大镜初始坐标
+
+    //横向
+    //鼠标水平不超过放大镜模块的一半时，放大镜不动
+    if (elementX.value < 100) position.x = 0;
+    //鼠标水平超过（显示图宽度-放大镜宽度+放大镜一半宽度）时，放大镜停止移动，防止出界
+    else if (elementX.value > 300) position.x = 200;
+    else position.x = elementX.value - 100;
+    
+    //纵向同理
+    if (elementY.value < 100) position.y = 0;
+    else if (elementY.value > 300) position.y = 200;
+    else position.y = elementY.value - 100;
+
+    // 给样式赋值
+    layerPosition.left = `${position.x}px`;
+    layerPosition.top = `${position.y}px`;
+});
+
 
 </script>
 
-
 <template>
-    <div class="image-preview flex">
-        <!-- 左侧大图 -->
-        <div class="main-image w-2/3 flex justify-center items-center bg-gray-100 p-4 relative">
-            <img id="mainImage" :src="imageList[activeIndex]" alt="" class="w-full h-auto object-contain">
-            <!-- 放大镜模块 -->
-            <div
-                class="magnifier absolute top-4 left-4 w-16 h-16 bg-gray-800 bg-opacity-50 text-white rounded-md flex justify-center items-center">
+    <div class="image-preview flex m-4">
+        <!-- 左侧显示图 -->
+        <div class="main-image relative w-[400px] h-[400px] flex justify-center items-center bg-gray-200 " ref="target"
+            :style="largePosition">
+            <img id="mainImage" :src="imageList[currIndex]" alt="" class="w-full h-full object-cover">
+            <!-- 放大镜 -->
+            <div v-show="show" class="magnifier absolute w-[200px] h-[200px] bg-gray-500 bg-opacity-50 overflow-hidden"
+                :style="layerPosition">
             </div>
         </div>
 
         <!-- 右侧缩略图 -->
-        <div class="thumbnails w-1/3 flex flex-col items-center p-4">
+        <div class="thumbnails w-[80px] flex flex-col items-center pl-4">
             <ul class="w-full space-y-4">
-                <li v-for="(image, i) in imageList" :key="i" @mouseenter="enterhandler(i)"
-                    :class="{ 'border-2 border-blue-500': activeIndex === i }"
-                    class="cursor-pointer flex justify-center">
-                    <img :src="image" alt="" class="w-20 h-20 object-cover">
+                <li v-for="(image, i) in imageList" :key="i"
+                    :class="['relative w-[68px] h-[68px] mb-[15px] border-2 cursor-pointer', { 'border-blue-500': i === currIndex }]"
+                    @mouseenter="currIndex = i">
+                    <img :src="image" alt="" class="w-full h-full object-cover">
                 </li>
             </ul>
         </div>
-
-
-        <!-- 放大镜大图盒子 -->
-        <div class="magnifier-box absolute hidden w-80 h-80 border border-gray-300 overflow-hidden bg-white shadow-lg">
-            <img id="magnifiedImage" :src="imageList[0]" alt="" class="absolute w-[800px] h-[800px] object-cover">
-        </div>
     </div>
+
 
 </template>
--- src/views/Detail/index.vue
+++ src/views/Detail/index.vue
@@ -20,7 +20,7 @@ onMounted(() => getGoods());
 <template>
     <div class="bg-gray-100 p-4">
         <!-- 面包屑 -->
-        <div class="bread-container mb-4 text-sm text-gray-500">
+        <div class="bread-container mb-6 ml-40 text-sm text-gray-500">
             <el-breadcrumb :separator-icon="ArrowRight">
                 <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
                 <el-breadcrumb-item :to="{ path: `/category/${goods.categories?.[1].id}` }">{{
@@ -32,52 +32,52 @@ onMounted(() => getGoods());
         </div>
 
         <!-- 大容器 -->
-        <div class="outer-container grid gap-4 p-4">
+        <div class="outer-container grid gap-4 px-40">
             <!-- 上部分 -->
-            <div class="top-container bg-white p-4 rounded shadow-md">
-                <div class="grid grid-cols-5 gap-5">
+            <div class="top-container bg-white rounded shadow-md">
+                <div class="grid grid-cols-7 gap-5 px-10">
                     <!-- 左上 -->
-                    <div class="top-left col-span-2 p-4 flex flex-col justify-center items-center">
+                    <div class="top-left col-span-3 p-4 flex flex-col justify-center ">
                         <!-- 图片预览区域 -->
                         <ImageView />
                         <!-- 统计数量 -->
-                        <div class="grid grid-cols-4 gap-2 w-full ">
+                        <div class="grid grid-cols-4 w-3/4 ml-8">
                             <div class="info-item text-center">
                                 <p class="title"><i class="icon">🔥</i>销量人气</p>
-                                <p class="data">{{ goods.salesCount }}+</p>
+                                <p class="data text-red-600 font-semibold">{{ goods.salesCount }}+</p>
                             </div>
                             <div class="info-item text-center">
                                 <p class="title"><i class="icon">⭐</i>商品评价</p>
-                                <p class="data">{{ goods.commentCount }}+</p>
+                                <p class="data text-red-600 font-semibold">{{ goods.commentCount }}+</p>
                             </div>
                             <div class="info-item text-center">
                                 <p class="title"><i class="icon">❤️</i>收藏人气</p>
-                                <p class="data">{{ goods.collectCount }}+</p>
+                                <p class="data text-red-600 font-semibold">{{ goods.collectCount }}+</p>
                             </div>
                             <div class="info-item text-center">
-                                <p class="title">品牌信息</p>
-                                <p class="data">{{ goods.brand?.name }}</p>
+                                <p class="title">😊品牌信息</p>
+                                <p class="data text-red-600 font-semibold">{{ goods.brand?.name }}</p>
                             </div>
                         </div>
                     </div>
                     <!-- 右上 -->
-                    <div class="top-right col-span-3 p-4">
+                    <div class="top-right col-span-4 p-4 m-4 space-y-4">
 
-                        <h1 class="text-xl font-bold text-black">{{ goods.name }}</h1>
-                        <p class="text-gray-600">{{ goods.desc }}</p>
-                        <p class="text-lg">
+                        <h1 class="text-3xl font-bold text-black">{{ goods.name }}</h1>
+                        <p class="text-lg text-gray-500">{{ goods.desc }}</p>
+                        <p class="text-2xl">
                             <span class="text-orange-500 font-bold">&yen;{{ goods.price }}</span>
                             <span class="line-through text-gray-500">&yen;{{ goods.oldPrice }}</span>
                         </p>
 
-                        <div class="text-sm text-gray-700">
-                            <p>配送方式：全国包邮</p>
-                            <p>售后保障：7天无理由退换货</p>
+                        <div class="text-lg text-gray-600 space-y-2 bg-gray-100 max-w-lg">
+                            <p class="pt-2 pl-2">配送方式：全国包邮</p>
+                            <p class="pb-2 pl-2">售后保障：7天无理由退换货</p>
                         </div>
 
                         <div>
                             <button
-                                class="w-max-fit p-2 bg-emerald-400 hover:bg-emerald-600 text-white rounded-lg text-lg">加入购物车</button>
+                                class="w-max-fit p-3 my-4 bg-emerald-400 hover:bg-emerald-600 text-white rounded-lg text-lg">加入购物车</button>
                         </div>
 
                     </div>
@@ -85,10 +85,10 @@ onMounted(() => getGoods());
             </div>
 
             <!-- 下部分 -->
-            <div class="bottom-container p-4 rounded shadow-md">
+            <div class="bottom-container rounded shadow-md">
                 <div class="grid grid-cols-8 gap-6">
                     <!-- 左下 -->
-                    <div class="bottom-left col-span-6 bg-white p-4 flex flex-col items-center">
+                    <div class="bottom-left col-span-6 bg-white p-14 flex flex-col items-center">
                         <!-- 商品详情部分 -->
                         <div class="details w-full mb-8">
                             <h3 class="text-xl font-bold mb-4">商品详情</h3>
